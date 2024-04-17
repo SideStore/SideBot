f98ec550394753fc35868fe4d4c1bb25d6c183dc
June P <rjp2030@outlook.com>
2024-04-17 17:17:50 +0900

    fix again

--- src/SideBot/db/tags.py
+++ src/SideBot/db/tags.py
@@ -30,7 +30,7 @@ class _Tags:
             CREATE TABLE IF NOT EXISTS tags (
                 guild_id BIGINT,
                 id SERIAL PRIMARY KEY NOT NULL UNIQUE,
-                name CITEXT NOT NULL,
+                name TEXT NOT NULL,
                 content TEXT NOT NULL,
                 author discorduser NOT NULL,
                 created_at TIMESTAMP DEFAULT NOW(),
